7dd823ee0bec8e78ced78a31cc04d3d1c7fb9b6a
S.Barzegari <barzegarisaeed.81@gmail.com>
2024-09-09 20:28:58 +0330

    Don't Show Export Code and Save in Different Mode

--- src/components/ERDTopBar.vue
+++ src/components/ERDTopBar.vue
@@ -15,7 +15,7 @@ function gridVisibleToggle(){
   <div id="navigator">
     <div class="top-bar">
       <div>
-        <span class="icon icon-red" @click="$emit('onOpenCode')">
+        <span v-if="route.name == 'editor'" class="icon icon-red" @click="$emit('onOpenCode')">
           <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24">
             <path d="M7 8L3 11.6923L7 16M17 8L21 11.6923L17 16M14 4L10 20" stroke-width="2"
                   stroke-linecap="round" stroke-linejoin="round"/>
